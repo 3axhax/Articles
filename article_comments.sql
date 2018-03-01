3924b8711a439a77006aa288ca5863f1a2632fb9
3axhax <3axhax@gmail.com>
2018-03-01 16:31:10 +0500

    view article
    Шаблон для комментариев. Форма для комментариев.

---
+++ article_comments.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Мар 01 2018 г., 14:29
+-- Версия сервера: 5.7.16
+-- Версия PHP: 7.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `article_comments`
+--
+CREATE DATABASE IF NOT EXISTS `article_comments` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `article_comments`;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `articles`
+--
+
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `articles`
+--
+
+INSERT INTO `articles` (`id`, `title`, `content`, `date`) VALUES
+(1, 'Язык программирования PHP', 'PHP — скриптовый язык общего назначения, интенсивно применяемый для разработки веб-приложений. В настоящее время поддерживается подавляющим большинством хостинг-провайдеров и является одним из лидеров среди языков, применяющихся для создания динамических веб-сайтов.', '2018-03-01 13:09:20'),
+(2, 'Язык программирования JavaScript', 'JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили. Является реализацией языка ECMAScript.', '2018-03-01 12:06:26');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `id_article` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `comment` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT для таблицы `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- views/article/view.php
+++ views/article/view.php
@@ -1,5 +1,5 @@
-<div class="media">
-    <div class="media-body">
+<div class="article">
+    <div class="article-body">
         <div class="panel panel-primary">
             <div class="panel-heading">
                 <div class="title"><h3><?= $article['title'] ?></h3></div>
@@ -11,7 +11,60 @@
                 <div class="media-text text-justify"><?= $article['content']?></div>
             </div>
         </div>
+    </div>
+</div>
+<hr>
+<div class="comment">
+    <div class="comment-body">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <div class="title"><h4>Автор</h4></div>
+                <div class="metadata">
+                    <span class="date">2018-00-00 13:00:00</span>
+                </div>
+            </div>
+            <div class="panel-body">
+                <div class="media-text text-justify">Comment</div>
+            </div>
         </div>
     </div>
-<pre>
-<?= print_r($article)?>
+</div>
+<hr>
+<div class="comment">
+    <div class="comment-body">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <div class="title"><h4>Добавить комментарий</h4></div>
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="form-horizontal col-md-12">
+                        <div class="form-group has-feedback">
+                            <label class="col-md-2">Имя:</label>
+                            <div class="col-md-3">
+                                <input type="text" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group has-feedback">
+                            <label class="col-md-2">E-mail:</label>
+                            <div class="col-md-3">
+                                <input type="email" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group has-feedback">
+                            <label class="col-md-2">Комментарий:</label>
+                            <div class="col-md-7">
+                                <textarea type="" class="form-control" rows="6"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-offset-2">
+                    <button type="submit" class="btn btn-success">Отправить <span class="glyphicon glyphicon-pencil"></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--<pre>
+--><?/*= print_r($article)*/?>
